f8604a8f0ff863a6b987153dc89836a2838f2373
김은영 <325242@naver.com>
2025-01-15 23:25:49 +0900

    공통 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./Notfound";
+import Button from "./Components/Button";
+import Header from "./Components/Header";
+
+import { getEmotionImage } from "./Util/get-emotion-image";
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary/:id" : 모든 일기를 조회하는 Diary 페이지
@@ -15,12 +20,15 @@ function App() {
   };
   return (
     <>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/new">New</Link>
-        <Link to="/diary">Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button text={"DEFAULT"} onClick={onClickButton} />
+      <Button text={"POSITIVE"} type="POSITIVE" onClick={onClickButton} />
+      <Button text={"NEGATIVE"} type="NEGATIVE" onClick={onClickButton} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
