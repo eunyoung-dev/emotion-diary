cfd13c42d1e89b388009d2f741a3b2ebb575432a
eunyoung-dev <eunyeong.dev@gmail.com>
2025-01-17 18:59:36 +0900

    Home 페이지 UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import Header from "../Components/Header";
+import Button from "../Components/Button";
+import DiaryList from "../Components/DiaryList";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        title={"2025년 1월"}
+        leftChild={<Button text={"<"} />}
+        rightChild={<Button text={">"} />}
+      />
+      <DiaryList />
+    </div>
+  );
 };
 
 export default Home;
