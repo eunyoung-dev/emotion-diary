4610024f36f7c2588abae485c96b962a69cced68
eunyoung-dev <eunyeong.dev@gmail.com>
2025-01-25 21:37:31 +0900

    New 페이지 UI 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,17 @@
+import Header from "../Components/Header";
+import Button from "../Components/Button";
+import Editor from "../Components/Editor";
+
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"}></Button>}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default New;
