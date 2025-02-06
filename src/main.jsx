30f9d6695e6a0d72e0d5332ab55a46d35459c875
eunyoung-dev <eunyeong.dev@gmail.com>
2025-02-06 15:21:02 +0900

    Edit 페이지 구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -7,9 +7,9 @@ import "./index.css";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </StrictMode>
 );
